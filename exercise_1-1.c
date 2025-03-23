d488d55126a23ceb7ab8cc5cfd167491916b866c
Cain <203079887+Abe-Cain@users.noreply.github.com>
2025-03-23 10:00:47 +0330

    exercise_1-1.c

---
+++ exercise_1-1.c
@@ -0,0 +1,16 @@
+/*Exercise 1-1. Run the ``hello, world'' program on your system. Experiment with leaving out parts of the program, to see what error messages you get.*/
+
+#include <stdio.h>
+
+int main() {
+    // 1. Missing closing quote and semicolon
+    printf(hello, world\n");
+
+    // 2. Missing semicolon at the end
+    printf("hello, world\n")
+
+    // 3. Incorrect closing parenthesis in function call
+    printf("hello, world\n";
+
+    return 0;
+}
