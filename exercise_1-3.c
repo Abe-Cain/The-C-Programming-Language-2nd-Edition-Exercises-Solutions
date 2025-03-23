8bde25f8bc106e4dc508d2054bf9cdb09dcf5058
Cain <203079887+Abe-Cain@users.noreply.github.com>
2025-03-23 10:16:29 +0330

    exercise_1-3.c

---
+++ exercise_1-3.c
@@ -0,0 +1,23 @@
+/*Exercise 1-3. Modify the temperature conversion program to print a heading above the table.*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Fahrenheit to Celsius Conversion Table\n\n");
+    printf("Fahrenheit\tCelsius\n");
+    printf("----------------------------\n");
+
+    float far, cel;
+    float min = 0, max = 200, step = 20;
+
+    far = min;
+
+    while (far <= max) {
+        cel = (5.0 / 9.0) * (far - 32.0);
+        printf("%6.1f\t\t%6.1f\n", far, cel);
+        far += step;
+    }
+
+    return 0;
+}
