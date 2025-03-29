e5d843e3556241dc167cfdb48dac42ac8eb9a52b
Cain <203079887+Abe-Cain@users.noreply.github.com>
2025-03-29 07:08:25 +0330

    exercise_1-16.c

--- exercise_1-16.c
+++ exercise_1-16.c
@@ -1,61 +1,44 @@
 #include <stdio.h>
 
-// A function to store a string and calculate its length
-int get_line(char array_to_hold_string[], int maximum_string_length);
-void copy(char to[], char from[]);
+#define MAX 2000
 
-#define MAX_LINE 1000
+int read_line(char str[], int limit);
+void print_line_length(int length);
 
-void main(void)
-{
-    char line[MAX_LINE];         
-    char longest_array[MAX_LINE];
-    int len;                     
-    int max = 0;                 
+int main() {
+    char line[MAX];
+    int lengths[MAX];
+    int length, count = 0;
 
-    while ((len = get_line(line, MAX_LINE)) > 0) {
-        if (len > max) {
-            max = len;
-            copy(longest_array, line); // Copy current longest line to longest_array
-        }
+    // Read lines and store their lengths
+    while ((length = read_line(line, MAX)) > 0) {
+        lengths[count++] = length;
     }
-    
-    if (max > 0) {
-        printf("Longest line: %s", longest_array); // Print the longest line
+
+    // Print all the lengths after processing all lines
+    for (int i = 0; i < count; i++) {
+        print_line_length(lengths[i] - 1);
     }
 
     return 0;
 }
 
-// Function to get a line of input
-int get_line(char char_string[], int lim)
-{
-    int current_char;
-    int index;
-
-    // Read characters until we reach the limit, EOF, or a newline character
-    for (index = 0; index < lim - 1 && ((current_char = getchar()) != EOF) && current_char != '\n'; ++index) {
-        char_string[index] = current_char;
+int read_line(char str[], int max_length_of_chars_in_line) {
+    int current_char = 0;
+    int i = 0;
+    for (i = 0; (i < max_length_of_chars_in_line - 1) && ((current_char = getchar()) != EOF) && (current_char != '\n'); ++i) {
+        str[i] = current_char;
     }
 
-    // If the character is a newline, include it in the array
     if (current_char == '\n') {
-        char_string[index] = current_char;
-        ++index;
+        str[i] = current_char;
+        ++i;
     }
 
-    // Null-terminate the string
-    char_string[index] = '\0';
-
-    return index;
+    str[i] = '\0';
+    return i;
 }
 
-// Function to copy the string from one array to another
-void copy(char to[], char from[])
-{
-    int index = 0;
-    // Copy each character from 'from' to 'to' until the null character is encountered
-    while ((to[index] = from[index]) != '\0') {
-        ++index;
-    }
+void print_line_length(int length) {
+    printf("Length: %d\n", length);
 }
